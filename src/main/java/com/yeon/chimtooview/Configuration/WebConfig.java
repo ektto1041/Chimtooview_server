349968099883fbd8baf89fd099e7ff1a0016d383
ektto1041 <dhkdwk1041@gmail.com>
2020-10-20 09:17:09 +0900

    CORSCORS

--- src/main/java/com/yeon/chimtooview/Configuration/WebConfig.java
+++ src/main/java/com/yeon/chimtooview/Configuration/WebConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://chimtooview.net").allowedOrigins("http://chimtooview.net/rughj2v9uiefj2q9eifjowefj"); // cors 에러 시 이거는 고정!
+        registry.addMapping("/**")
+                .allowedOrigins("http://chimtooview.net")
+                .allowedMethods("*"); // cors 에러 시 이거는 고정!
     }
 }
